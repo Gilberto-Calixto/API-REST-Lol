5ca551df18ccc6b4412b332a21c5484b14743b69
AloneBoyy <gilbertocalixto2002@gmail.com>
2024-03-25 19:58:09 -0300

    Projeto Inicial

---
+++ src/main/java/com/dev/santande_dev_wk/SantandeDevWkApplication.java
@@ -0,0 +1,13 @@
+package com.dev.santande_dev_wk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantandeDevWkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantandeDevWkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/santande_dev_wk/SantandeDevWkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.santande_dev_wk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantandeDevWkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
